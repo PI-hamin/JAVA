267c2a78beb1ace748eb92f2a9cf15945e0ef517
PI-hamin <76045212+PI-hamin@users.noreply.github.com>
2021-11-06 00:16:19 +0900

    Add files via upload

---
+++ HellowWorld.java
@@ -0,0 +1,11 @@
+package java01;
+
+public class HellowWorld {
+
+	public static void main(String[] args) { // 메인함수이며 우선으로 실행되는 메소드
+		// TODO Auto-generated method stub
+		System.out.println("java공부");//로그출력
+		// print 는 줄바꿈을 안하는 것이고 println 은 줄바꿈. 
+	}
+
+}
