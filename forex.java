7037a526145f416c05a73008e73819edd65c224a
PI-hamin <76045212+PI-hamin@users.noreply.github.com>
2021-11-07 20:12:18 +0900

    for 문 연습

---
+++ forex.java
@@ -0,0 +1,18 @@
+package java04;
+
+public class forex {
+	public static void main(String[]args) {
+		
+		String str[] = {"하나","둘","셋"};
+		
+		for (int i=0; i<3; i++) {
+		System.out.println("안녕하세요");
+		}
+		System.out.println("마칩니다");
+		
+		for (int f=0; f<str.length; f++) {
+			System.out.println("하나");
+		}
+	}
+
+}
